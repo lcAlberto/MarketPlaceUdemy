6494e18bc7e3624968e791d0bcb05ba77a44a9dd
Lucka190697 <lucasmatoso@letsgrow.com.br>
2021-03-28 15:23:32 -0300

    Leyout Blade, Geracao dos controllers e rotas

---
+++ MarketPlace/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MarketPlace UDEMY</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="conteiner p-3">
+    <div class="card">
+        <div class="card-header">
+            LOJA
+        </div>
+        <div class="card-body">
+            <div class="col-12">
+                @yield("content")
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
