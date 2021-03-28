26ff565f80ce2a96228c738ceebe56efb1742978
Lucka190697 <lucasmatoso@letsgrow.com.br>
2021-03-28 18:22:44 -0300

    Criaao dos metodos editar e excluir loja, adicao de icones fontawesome e criacao de mensagens de retorno

--- MarketPlace/resources/views/admin/stores/index.blade.php
+++ MarketPlace/resources/views/admin/stores/index.blade.php
@@ -2,25 +2,62 @@
 
 @section('content')
 
-    <table class="table table table-striped">
-        <thead>
-        <tr>
-            <th>id</th>
-            <th>Loja</th>
-            <th>Ações</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($stores as $store)
-            <tr>
-                <td>{{$store->id}}</td>
-                <td>{{$store->name}}</td>
-                <td></td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+    <div class="card">
+        <div class="card-header">
+            <div class="col-9 float-left">
+                Lojas
+            </div>
+            <div class="col-3 float-right">
+                <a
+                    href="{{ route('admin.store.create') }}"
+                    class="btn btn-block btn-primary">
+                    <i class="fa fa-plus mr-2"></i>
+                    Criar nova Loja
+                </a>
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table table table-striped">
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>thumbnail</th>
+                    <th>Loja</th>
+                    <th>Descrição</th>
+                    <th>Telefone</th>
+                    <th>Ações</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($stores as $store)
+                    <tr>
+                        <td>{{$store->id}}</td>
+                        <td>{{$store->thumbnail ? $store->thumbnail : 'Default.jpg' }}</td>
+                        <td>{{$store->name}}</td>
+                        <td>{{$store->description}}</td>
+                        <td>{{$store->phone}}</td>
+                        <td class="btn-group">
+                            <a
+                                href="{{ route('admin.store.edit', $store->id) }}"
+                                class="btn btn-success">
+                                <i class="fa fa-pencil mr-2"></i>
+                                Editar
+                            </a>
+                            <a
+                                href="{{ route('admin.store.destroy', $store->id) }}"
+                                class="btn btn-danger">
+                                <i class="fa fa-trash mr-2"></i>
+                                Excluir
+                            </a>
 
-    {{$stores->links()}}
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+            {{$stores->links()}}
+        </div>
+    </div>
 
 @endsection
